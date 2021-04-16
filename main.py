8a5edbd7bce5ac5b489155fc7880b2601ba32c24
Alexa Tartaglini <genghis0swan@gmail.com>
2021-04-16 10:00:14 -0400

    Updated code to analyze number of shape and texture decisions, added plots

--- main.py
+++ main.py
@@ -6,49 +6,163 @@ import copy
 import os
 import numpy as np
 import shutil
+import pandas as pd
 import probabilities_to_decision
 import helper.human_categories as sc
 import matplotlib.pyplot as plt
+from matplotlib import cm
 from dataloader import GeirhosStyleTransferDataset
 
-def transform_data(path, batch):
-    # Using more or less the same preprocessing as Emin (but without center crop)
-    preprocess = transforms.Compose([
-        transforms.Resize(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
 
-    image_datasets = datasets.ImageFolder(path, preprocess)
-    dataloaders = torch.utils.data.DataLoader(image_datasets, batch_size=batch,
-                                                    shuffle=False)
-    dataset_sizes = len(image_datasets)
+def plot_class_values(categories, class_values, im, shape, texture):
+    """This function plots the values that the model assigns to the Geirhos
+    Style Transfer classes (airplane, bear, ..., oven, truck; 16 total).
 
-    return dataloaders, dataset_sizes
+    :param categories: a list of the 16 Geirhos classes, either organized by shape or
+        texture.
+    :param class_values: A length 16 vector. The values referred to here are those
+        calculated by the Geirhos probability mapping code, which takes the 1000-length
+        vector output of the model as an input; it then groups the various ImageNet classes
+        into groups that correspond with a single Geirhos class, and it takes the average of
+        the probabilities amongst this group of ImageNet classes. This average becomes
+        the value assigned to the Geirhos class, and the class receiving the highest average
+        probability is the model's decision.
+    :param im: the path of the image file that produced these results.
+    :param shape: the shape classification of the given image.
+    :param texture: the texture classification of the given image."""
+
+    decision_idx = class_values.index(max(class_values))  # index of maximum class value
+    decision = categories[decision_idx]
+    shape_idx = categories.index(shape)  # index of shape category
+    texture_idx = categories.index(texture)  # index of texture category
+
+    spec = plt.GridSpec(ncols=2, nrows=1, width_ratios=[4, 1], wspace=0.2, )
+
+    fig = plt.figure()
+    fig.set_figheight(6)
+    fig.set_figwidth(9.5)
+
+    # Bar plot
+    fig.add_subplot(spec[0])
+    plt.bar(categories, class_values, color=(0.4, 0.4, 0.4), width=0.4)
+    plt.bar(categories[decision_idx], class_values[decision_idx],
+            color=(0.9411764705882353, 0.00784313725490196, 0.4980392156862745), width=0.4)
+    plt.bar(categories[shape_idx], class_values[shape_idx],
+            color=(0.4980392156862745, 0.788235294117647, 0.4980392156862745), width=0.4)
+    plt.bar(categories[texture_idx], class_values[texture_idx],
+            color=(0.7450980392156863, 0.6823529411764706, 0.8313725490196079), width=0.4)
+    plt.xlabel("Geirhos Style Transfer class", fontsize=12)
+    plt.ylabel("Average probabilities across associated ImageNet classes", fontsize=10)
+    plt.suptitle("Model decision for " + im + ":", fontsize=15)
+    plt.title("Model Outputs", fontsize=12)
+    plt.xticks(rotation=45)
+    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+
+    # Create the legend
+    colors = {'model decision: ' + decision: (0.9411764705882353, 0.00784313725490196, 0.4980392156862745),
+              'shape category: ' + shape: (0.4980392156862745, 0.788235294117647, 0.4980392156862745),
+              'texture category: ' + texture: (0.7450980392156863, 0.6823529411764706, 0.8313725490196079)}
+    labels = list(colors.keys())
+    handles = [plt.Rectangle((0, 0), 1, 1, color=colors[label]) for label in labels]
+    plt.legend(handles, labels)
+
+    # Plot the image
+    im_ax = fig.add_subplot(spec[1])
+    img = plt.imread('stimuli-shape/style-transfer/' + shape + '/' + im)
+    plt.imshow(img)
+    plt.title(im)
+    im_ax.set_xticks([])
+    im_ax.set_yticks([])
+
+    plt.savefig('figures/' + im)
+
+
+def csv_class_values(shape_dict, shape_categories):
+    columns = ['Shape', 'Texture', 'Decision', 'Shape Category Value',
+               'Texture Category Value', 'Decision Category Value',
+               'Shape Decision', 'Texture Decision', 'Neither']
+
+    for shape in shape_categories:
+        df = pd.DataFrame(index=range(len(shape_categories)), columns=columns)
+        df['Shape'] = shape
+
+        for i, row in df.iterrows():
+            texture = shape_categories[i]
+            decision = shape_dict[shape][texture + '0'][0]
+            class_values = shape_dict[shape][texture + '0'][1]
+
+            row['Texture'] = texture
+            row['Decision'] = decision
+            row['Shape Category Value'] = class_values[shape_categories.index(shape)]
+            row['Texture Category Value'] = class_values[shape_categories.index(texture)]
+            row['Decision Category Value'] = class_values[shape_categories.index(decision)]
+
+            row['Shape Decision'] = int(decision == shape)
+            row['Texture Decision'] = int(decision == texture)
+            row['Neither'] = int(decision != shape and decision != texture)
+
+        df.to_csv('csv/' + shape + '.csv', index=False)
+
+
+def calculate_totals(shape_categories):
+    shape_dict = dict.fromkeys(shape_categories)
+    texture_dict = dict.fromkeys(shape_categories)
+    neither_dict = dict.fromkeys(shape_categories)
+    for shape in shape_categories:
+        shape_dict[shape] = 0
+        texture_dict[shape] = 0
+        neither_dict[shape] = 0
+
+    for filename in os.listdir('csv'):
+        df = pd.read_csv('csv/' + filename)
+        shape = df['Shape'][0]
+        for i, row in df.iterrows():
+            shape_dict[shape] = shape_dict[shape] + row['Shape Decision']
+            texture_dict[shape] += row['Texture Decision']
+            neither_dict[shape] += row['Neither']
+
+    for shape in shape_categories:
+        print("Shape category: " + shape)
+        print("\tNumber shape decisions: " + str(shape_dict[shape]))
+        print("\tNumber texture decisions: " + str(texture_dict[shape]))
+        print("\tNumber neither shape nor texture decisions: " + str(neither_dict[shape]))
+        print()
+
+    print("IN TOTAL:")
+    print("\tNumber shape decisions: " + str(sum(shape_dict.values())))
+    print("\tNumber texture decisions: " + str(sum(texture_dict.values())))
+    print("\tNumber neither shape nor texture decisions: " + str(sum(neither_dict.values())))
+
+
 
 
 if __name__ == '__main__':
 
     batch_size = 1
-    shape_categories = sc.get_human_object_recognition_categories() # list of 16 classes in the Geirhos style-transfer dataset
+    shape_categories = sc.get_human_object_recognition_categories()  # list of 16 classes in the Geirhos style-transfer dataset
     shape_dir = 'stimuli-shape/style-transfer'
     texture_dir = 'stimuli-texture/style-transfer'
+    plot = False
+    verbose = True
 
-    # Load Emin's pretrained SAYCAM model from the .tar file. I'm using more or less
-    # the same code that Emin uses in imagenet_finetuning.py
+    '''
+    shape_dict = dict.fromkeys(shape_categories)  # for storing the results
+    shape_categories0 = [shape + '0' for shape in shape_categories]
+    shape_dict0 = dict.fromkeys(shape_categories0)
+    for shape in shape_categories:
+        shape_dict[shape] = shape_dict0
+
+    shape_dict['airplane']['airplane'] = 0
+
+    # Load Emin's pretrained SAYCAM model + ImageNet classifier from its .tar file
     model = models.resnext50_32x4d(pretrained=True)
-    model.fc = torch.nn.Linear(in_features=2048, out_features=2765, bias=True)
-    model = torch.nn.DataParallel(model) # What does this do?
-    checkpoint = torch.load('models/resnext50_S.tar', map_location=torch.device('cpu'))
+    model.fc = torch.nn.Linear(in_features=2048, out_features=1000, bias=True)
+    model = torch.nn.DataParallel(model)
+    checkpoint = torch.load('models/fz_IN_resnext50_32x4d_augmentation_True_SAY_5_288.tar',
+                            map_location=torch.device('cpu'))
     model.load_state_dict(checkpoint['model_state_dict'])
 
-    # Add an ImageNet classifier. Not sure if I'm doing all of this correctly
-    model.module.fc = torch.nn.Linear(in_features=2048, out_features=1000, bias=True)
-
-    # Process the images
-    #images, size = transform_data('stimuli-shape/style-transfer/', batch_size)
-
-    # Load and process the images
+    # Load and process the images using my custom Geirhos style transfer dataset class
     g = GeirhosStyleTransferDataset(shape_dir, texture_dir)
 
     # Obtain ImageNet - Geirhos mapping
@@ -65,28 +179,16 @@ if __name__ == '__main__':
 
         decision, class_values = mapping.probabilities_to_decision(soft_output)
 
-        print('decision for ' + im_dir + ': ' + decision)
-        #for j in range(16):
-            #print(shape_categories[j] + ': ' + str(class_values[j]))
-        #print()
+        if verbose:
+            print('decision for ' + im_dir + ': ' + decision)
+            # for j in range(16):
+            # print(shape_categories[j] + ': ' + str(class_values[j]))
+            # print()
+        if plot:
+            plot_class_values(shape_categories, class_values, im_dir, shape, texture)
 
+        shape_dict[shape][texture + '0'] = [decision, class_values]
 
-'''
-    # Pass the images through the model in batches of 1
-    for i, (image_batch, target) in enumerate(images):
-        output = model(image_batch) # 1 1000-length tensor
-        soft_outputs = softmax(output)
-        decisions = []
-
-        for j in range(batch_size):
-            output = soft_outputs[j].detach().numpy()
-            decision, probabilities = mapping.probabilities_to_decision(output)
-            decisions.append(decision)
-
-            #print("Category probabilities (16 Geirhos classes) for image " + str(i) + ": ")
-            #for k in range(len(shape_categories)):
-                #print(shape_categories[k] + ': ' + str(probabilities[k]))
-
-        print("decision: " + decision)
-        #print(target.item())
-'''
+    '''
+    #csv_class_values(shape_dict, shape_categories)
+    calculate_totals(shape_categories)
