f933e7d6e09dd9eabfdf50d3c9e66b2400d9c056
Alexa Tartaglini <genghis0swan@gmail.com>
2021-05-08 19:26:07 -0400

    fixed issues with dataloader, added serialized dictionary for dataset

--- data.py
+++ data.py
@@ -3,6 +3,8 @@ from torch.utils.data import Dataset
 from torchvision import transforms
 from PIL import Image
 import shutil
+import json
+import glob
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -21,7 +23,6 @@ class GeirhosStyleTransferDataset(Dataset):
         self.shape_dir = shape_dir
         self.texture_dir = texture_dir
         self.shape_classes = {}
-        self.texture_classes = {}
 
         # Default image processing
         if transform is None:
@@ -33,16 +34,31 @@ class GeirhosStyleTransferDataset(Dataset):
         else:
             self.transform = transform
 
-        # Create dictionaries containing shape and texture classifications for each image
-        for category in sorted(os.listdir(shape_dir)):
-            if category != '.DS_Store':
-                for image in sorted(os.listdir(shape_dir + '/' + category)):
-                    self.shape_classes[image] = category
-                for image in sorted(os.listdir(texture_dir + '/' + category)):
-                    self.texture_classes[image] = category
+        # Create/load dictionaries containing shape and texture classifications for each image
+        try:
+            # Load dictionary
+            self.shape_classes = json.load(open('geirhos_shape_classes.json'))
 
-        if '.DS_Store' in self.shape_classes.keys():
-            self.shape_classes.pop('.DS_Store')
+        except FileNotFoundError:
+            # Create dictionary
+            for image_dir in glob.glob('stimuli-shape/style-transfer/*/*.png'):
+                image = image_dir.split('/')
+                shape = image[2]  # Shape class of image
+                texture_spec = image[3].split('-')[1].replace('.png', '')  # Specific texture instance, eg. clock2
+                shape_spec = image[3].split('-')[0]  # Specific shape instance, eg. airplane1
+                texture = ''.join([i for i in texture_spec if not i.isdigit()])  # Texture class
+
+                if shape != texture:  # Filter images that are not cue-conflict
+                    self.shape_classes[image[3]] = {}  # Initialize dictionary for single image
+                    self.shape_classes[image[3]]['shape'] = shape
+                    self.shape_classes[image[3]]['texture'] = texture
+                    self.shape_classes[image[3]]['shape_spec'] = shape_spec
+                    self.shape_classes[image[3]]['texture_spec'] = texture_spec
+                    self.shape_classes[image[3]]['dir'] = image_dir
+
+            # Save dictionary as a JSON file
+            with open('geirhos_shape_classes.json', 'w') as file:
+                json.dump(self.shape_classes, file)
 
     def __len__(self):
         """
@@ -59,23 +75,25 @@ class GeirhosStyleTransferDataset(Dataset):
         specific texture.
         """
 
-        images = [key for key in self.shape_classes.keys()]
-        image_dir = self.shape_dir + '/' + self.shape_classes[images[idx]] + '/' + images[idx]
-        image = Image.open(image_dir)
+        images = sorted([key for key in self.shape_classes.keys()])
+
+        image_name = images[idx]  # Name of PNG file
+        im_dict = self.shape_classes[image_name]  # Dictionary with properties for this image
+        image_dir = im_dict['dir']  # Full path to image file
+
+        image = Image.open(image_dir)  # Load image
 
         #image.show()
 
         if self.transform:
             image = self.transform(image)
 
-        shape = self.shape_classes[images[idx]]
-        texture = self.texture_classes[images[idx]]
+        shape = im_dict['shape']
+        texture = im_dict['texture']
+        shape_spec = im_dict['shape_spec']
+        texture_spec = im_dict['texture_spec']
 
-        spec = images[idx][:-4:].split('-')
-        shape_spec = spec[0]  # Specific shape, eg. airplane1
-        texture_spec = spec[1]  # Specific texture, eg. clock2
-
-        return image, images[idx], shape, texture, shape_spec, texture_spec
+        return image, image_name, shape, texture, shape_spec, texture_spec
 
     def create_texture_dir(self, shape_dir, texture_dir):
         """Takes a dataset that is organized by shape category and copies
